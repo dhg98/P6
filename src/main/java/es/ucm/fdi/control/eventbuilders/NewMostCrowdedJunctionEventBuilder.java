479ebd227b7e1a8ae354073dae3b936e66617ea0
javigrh <javigrh@gmail.com>
2018-03-16 17:42:09 +0100

    Cruces avanzados. Implementacion parcial de las clases relacionadas con los cruces avanzados.

---
+++ src/main/java/es/ucm/fdi/control/eventbuilders/NewMostCrowdedJunctionEventBuilder.java
@@ -0,0 +1,27 @@
+package es.ucm.fdi.control.eventbuilders;
+
+import es.ucm.fdi.ini.IniSection;
+import es.ucm.fdi.model.*;
+
+public class NewMostCrowdedJunctionEventBuilder implements EventBuilder {
+
+	@Override
+	public Event parse(IniSection sec) throws IllegalArgumentException {
+		if(!"new_junction".equals(sec.getTag()) || !"mc".equals(sec.getValue("type"))){
+			return null;
+		} else {
+			try {
+				int time = this.parseInt(sec, "time", 0);
+				if (isValidId(sec.getValue("id"))){
+					return new NewMostCrowdedJunctionEvent(time, sec.getValue("id"));
+				} else {
+					throw new IllegalArgumentException("The id you´ve tried to parse contains invalid characters.");
+				}
+				
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException("The time you've given is negative", e);
+			}
+		}
+	}
+
+}
---
+++ src/main/java/es/ucm/fdi/control/eventbuilders/NewRoundRobinJunctionEventBuilder.java
@@ -0,0 +1,29 @@
+package es.ucm.fdi.control.eventbuilders;
+
+import es.ucm.fdi.ini.IniSection;
+import es.ucm.fdi.model.*;
+
+public class NewRoundRobinJunctionEventBuilder implements EventBuilder {
+
+	@Override
+	public Event parse(IniSection sec) throws IllegalArgumentException {
+		if(!"new_junction".equals(sec.getTag()) || !"rr".equals(sec.getValue("type"))){
+			return null;
+		} else {
+			try {
+				int time = this.parseInt(sec, "time", 0);
+				int maxSlice = this.parseInt(sec, "max_time_slice", 1);
+				int minSlice = this.parseInt(sec, "min_time_slice", 1);
+				if (isValidId(sec.getValue("id"))){
+					return new NewRoundRobinJunctionEvent(time, sec.getValue("id"), maxSlice, minSlice);
+				} else {
+					throw new IllegalArgumentException("The id you´ve tried to parse contains invalid characters.");
+				}
+				
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException("The time you've given is negative", e);
+			}
+		}
+	}
+
+}
