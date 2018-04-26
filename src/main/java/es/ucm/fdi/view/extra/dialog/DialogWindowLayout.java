7300eaff899ff1ab53027724bf067489784430b7
javigrh <javigrh@gmail.com>
2018-04-26 10:59:24 +0200

    Modificaciones menores

--- src/main/java/es/ucm/fdi/view/extra/dialog/DialogWindowLayout.java
+++ src/main/java/es/ucm/fdi/view/extra/dialog/DialogWindowLayout.java
@@ -42,7 +42,6 @@ public class DialogWindowLayout extends JFrame {
     private void initGUI() {
 		_dialog = new DialogWindow(this);
 		_dialog.setData(_vehicles, _roads, _junctions);
-//		this.setVisible(true);
 		status = _dialog.open();
         if (status != 0) {
             simulator.fillReport(_dialog.getSelectedVehicles(), ini);
@@ -50,7 +49,7 @@ public class DialogWindowLayout extends JFrame {
             simulator.fillReport(_dialog.getSelectedJunctions(), ini);
         }
 		
-//		this.pack();
+        //this.pack();
 		
 	}
 }
